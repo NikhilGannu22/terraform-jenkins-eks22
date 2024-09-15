0283ac2da1a4ceeefadc99d8d91a200c871b5426
Nikhil Gannu <nikhilgannu@sosonweb.com>
2024-09-15 12:39:37 -0400

    adding config file

--- EKS cluster/backend.tf
+++ EKS cluster/backend.tf
@@ -1,9 +1,9 @@
-terraform{
-    backend "s3" {
-        bucket = "cicdterraform22"
-        key = "eks/terraform.tfstate"
-        region = "us-east-1"
-        
-      
-    }
+terraform {
+  backend "s3" {
+    bucket = "cicdterraform22"
+    key    = "eks/terraform.tfstate"
+    region = "us-east-1"
+
+
+  }
 }
